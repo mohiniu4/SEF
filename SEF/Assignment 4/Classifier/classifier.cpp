4628aa519e947ce358e34d307a0b519f0d22596f
mohiniu4 <110068413+mohiniu4@users.noreply.github.com>
2022-12-07 17:48:41 -0500

    Add files via upload

---
+++ SEF/Assignment 4/Classifier/classifier.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void train() {
+	cout << "Train\n";
+}
+
+void predict() {
+	cout << "Predict\n";
+}
+
+// main() is where program execution begins.
+int main() {
+	train();
+	predict();
+	return 0;
+}
