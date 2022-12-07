b736ed10659339e1c2f6e2d2376902eb4887f5a9
umang <umohini4648@conestogac.on.ca>
2022-12-07 15:11:22 -0500

    train function added

---
+++ classifier.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void train() {
+	cout << "Train";
+}
+
+//void predict() {
+//	cout << "Predict";
+//}
+
+// main() is where program execution begins.
+int main() {
+	train();
+	//predict();
+	return 0;
+}
